63a056e7adce3beae7911fcc86aa6dd2aab04890
Paxxs <10364775+Paxxs@users.noreply.github.com>
2021-02-02 23:44:57 +0800

    item 鼠标移入移出显示

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -4,15 +4,26 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export default class Item extends Component {
+    state = {
+        mouseOn: false
+    }
     static propsTypes = {
         id: PropTypes.string.isRequired,
         name: PropTypes.string.isRequired,
         done: PropTypes.bool.isRequired
     }
+    // 检测鼠标是否移入，更新状态
+    handleMousehover = (mouseOn) => {
+        return () => {
+            this.setState({
+                mouseOn
+            })
+        }
+    }
     render() {
         const { name, done } = this.props
         return (
-            <li>
+            <li onMouseEnter={this.handleMousehover(true)} onMouseLeave={this.handleMousehover(false)}>
                 <label>
                     <input type="checkbox" defaultChecked={done} />
                     <span>{name}</span>
@@ -20,7 +31,7 @@ export default class Item extends Component {
                 <button
                     // className="btn btn-danger" 
                     className={`${styles.btn} ${styles.btn_danger}`}
-                    style={{ display: 'none' }}>删除</button>
+                    style={{ display: this.state.mouseOn ? 'block' : 'none' }}>删除</button>
             </li>
         )
     }
