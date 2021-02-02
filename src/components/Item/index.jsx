1d76ec4a6a7cc5c1c0301d805b656b5edfc30782
Paxxs <10364775+Paxxs@users.noreply.github.com>
2021-02-02 23:19:53 +0800

    Item 组件通过props获取值显示内容

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 // import './index.css'
 import styles from './index.module.css'
 
 export default class Item extends Component {
+    static propsTypes = {
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        done: PropTypes.bool.isRequired
+    }
     render() {
+        const { name, done } = this.props
         return (
             <li>
                 <label>
-                    <input type="checkbox" />
-                    <span>xxxxx</span>
+                    <input type="checkbox" defaultChecked={done} />
+                    <span>{name}</span>
                 </label>
                 <button
                     // className="btn btn-danger" 
