2349fe9e55d176f774a746d146ba7f3fe20ef6f9
Paxxs <10364775+Paxxs@users.noreply.github.com>
2021-02-02 23:13:55 +0800

    添加App初始state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { nanoid } from 'nanoid'
 // import './App.css'
 import sytles from './App.module.css'
 import Footer from './components/Footer';
@@ -6,6 +7,24 @@ import Header from './components/Header';
 import List from './components/List';
 
 class App extends Component {
+  state = {
+    todos: [
+      {
+        id: nanoid(),
+        name: "吃饭",
+        done: true
+      }, {
+        id: nanoid(),
+        name: "睡觉",
+        done: true
+      }, {
+        id: nanoid(),
+        name: "打豆豆",
+        done: true
+      }
+    ]
+  }
+
   render() {
     return (
       <div className={sytles.todo_container}>
